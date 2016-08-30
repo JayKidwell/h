a1c06a8ffde93b5789def2eb824b2e0ce9339a5b
Jay Kidwell <jay@thekidwells.net>
2016-08-29 21:41:52 -0400

    working

--- motorPwmDemo/motorPwmDemo.ino
+++ motorPwmDemo/motorPwmDemo.ino
@@ -1,8 +1,4 @@
 /*
- Fading
-
- This example shows how to fade an LED using the analogWrite() function.
-
  The circuit:
  * using L298N H-bridge board
  * hbridge 
@@ -10,55 +6,83 @@
  * pin 10 PWM - to H-bridge In2 (motor A, reverse)
  */
 //
-int forwardPin = 9;
-int reversePin = 10;
+int rightForwardPin = 5;
+int rightReversePin = 6;
+int leftForwardPin = 9;
+int leftReversePin = 10;
 int maxValue = 255; /* 1-255 */
 int fadeSpeed = 25; /* 1-255, lower is slower, 5 typical */
 int pauseTime = 500; /* msec to pause between forward and reverse */
-int runTime = 1000; /* msec to run full speed before slowing */
+int runTime = 2000; /* msec to run full speed before slowing */
+// ==========================================================================================
+// setup
 //
 void setup() {
-  analogWrite(reversePin, 0);
-  analogWrite(forwardPin, 0);
+  moveStop();
 }
-
+// ==========================================================================================
+// loop
+// 
+// each attached device has a currentLevel, a targetLevel, and an acceleration.
+// changes are made by modifying the targetLevel, and optionally the acceperation
+// the loop translates those changes into motion changes
+//
 void loop() {
   //
   // forward - ramp from off to full
   //
   for (int fadeValue = 0 ; fadeValue <= maxValue; fadeValue += fadeSpeed) {
-    analogWrite(forwardPin, fadeValue);
+    analogWrite(leftForwardPin, fadeValue);
+    analogWrite(rightForwardPin, fadeValue);
     delay(30);
   }
-  analogWrite(forwardPin, maxValue);
+  analogWrite(leftForwardPin, maxValue);
+  analogWrite(rightForwardPin, maxValue);
   delay(runTime);
   //
   // forward - ramp from full to off
   //
   for (int fadeValue = maxValue ; fadeValue >= 0; fadeValue -= fadeSpeed) {
-    analogWrite(forwardPin, fadeValue);
+    analogWrite(leftForwardPin, fadeValue);
+    analogWrite(rightForwardPin, fadeValue);
     delay(30);
   }
-  analogWrite(forwardPin, 0);
+  moveStop();
   delay(pauseTime);
   //
   // reverse - ramp from off to full
   //
   for (int fadeValue = 0 ; fadeValue <= maxValue; fadeValue += fadeSpeed) {
-    analogWrite(reversePin, fadeValue);
+    analogWrite(leftReversePin, fadeValue);
+    analogWrite(rightReversePin, fadeValue);
     delay(30);
   }
-  analogWrite(reversePin, maxValue);
+  analogWrite(leftReversePin, maxValue);
   delay(runTime);
   //
   // reverse - ramp from full to off
   //
   for (int fadeValue = maxValue ; fadeValue >= 0; fadeValue -= fadeSpeed) {
-    analogWrite(reversePin, fadeValue);
+    analogWrite(leftReversePin, fadeValue);
+    analogWrite(rightReversePin, fadeValue);
     delay(30);
   }
-  analogWrite(reversePin, 0);
+  moveStop();
   delay(pauseTime);
 }
-
+/* ======================================================================
+ * Stop 
+*/
+void moveStop (){
+  analogWrite(leftForwardPin, 0);
+  analogWrite(leftReversePin, 0);
+  analogWrite(rightForwardPin, 0);
+  analogWrite(rightReversePin, 0);
+}
+/* ======================================================================
+ * Forward 
+*/
+void moveForward() {
+  
+}
 
